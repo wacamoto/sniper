3b83c49d1a7f6d34e83fe2ad9245d7a958ef4890
waka <waka@example.com>
2015-12-06 01:16:40 +0800

    defined sniper

---
+++ src/js/draw.js
@@ -0,0 +1,2 @@
+s = Snap('#gameWindows');
+var bigCircle = s.circle(150, 150, 100);
--- src/js/event.js
+++ src/js/event.js
@@ -1,9 +1,74 @@
+//all buliding & road
+
+buliding = {
+    "chruch":{
+        "widows": 4,
+        "locate": "",
+    },
+    "house":{
+        "widows": 4,
+        "locate": "",
+    },
+    "hospital":{
+        "widows": 4,
+        "locate": "",
+    },
+    "apartment":{
+        "widows": 4,
+        "locate": "",
+    },
+    "villa":{
+        "widows": 4,
+        "locate": "",
+    },
+}
+
+
+sniper = function() {
+    this.role;
+    this.position;
+    this.hideing;
+    this.looking;
+    this.aiming;
+    this.moving;
+    this.defence;
+    this.killTimes;
+    this.energy;
+    this.moveCount;
+}
+
+sniper.prototype.hide = function() {}
+sniper.prototype.look = function() {}
+sniper.prototype.aim = function() {}
+sniper.prototype.fire = function() {}
+sniper.prototype.move = function() {}
+sniper.prototype.defence = function() {}
+
+sniper.prototype.map = function() {}
+sniper.prototype.watch = function() {}
+sniper.prototype.knife = function() {}
+sniper.prototype.sniperGun = function() {}
+sniper.prototype.smoke = function() {}
+
+sniper.prototype.update = function() {}
+
+
+
 var events = {};
 
 events.connectReq = connectReq;
 events.startGame = startGame;
 events.message = message;
 events.chat = chat;
+
+
+// events.newRound = newRound;
+
+// events.die = die;
+// events.dawn = dawn;
+
+
+
 
 function connectReq(data) {
     another_peer_id = data.id;
@@ -19,6 +84,8 @@ function startGame(data) {
     $('#gameWindows').show();
 }
 
+function newReound() {
+}
 
 function chat(data) {
     console.log(data.detail);
